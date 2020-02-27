fb2dd2d00663617d7a4ebd4786ce2b16640c7e9d
Ratul Chowdhury <ratul_chowdhury@hms.harvard.edu>
2020-02-27 17:54:39 -0500

    Add files via upload
    If your MSA alignment file (.aln) or (.a3m) has gaps then trRosetta cannot be used to predict structure
    
    This is a python code which makes the distogram .npz object compatible of structure prediction using trRosetta

---
+++ fix_o_disto.py
@@ -0,0 +1,43 @@
+import sys,os
+import numpy as np
+
+name_a3m=input('Enter a3m alignment filename (add .a3m extension)': )
+name_npz=input('Enter the npz file you obtained from executing networks/predict.py (add .npz extension)': )
+
+
+a3m_open=open(str(name_a3m))
+a3m_read=a3m_open.readlines()
+
+ref_seq=str(a3m_read[1]).strip()
+
+
+a=()
+
+for i in range(len(ref_seq)):
+    if str(ref_seq[i])=='-':
+        a+=(int(i),)
+
+npz=np.load(str(name_npz))
+
+dis=np.delete(npz['dist'],a,0)
+dis=np.delete(dis,a,1)
+
+thet=np.delete(npz['theta'],a,0)
+thet=np.delete(thet,a,1)
+
+omega=np.delete(npz['omega'],a,0)
+omega=np.delete(omega,a,1)
+
+phi=np.delete(npz['phi'],a,0)
+phi=np.delete(phi,a,1)
+
+
+#temp={'dist':dis, 'theta':thet, 'omega':omega, 'phi':phi}
+
+np.savez('corrected_'+str(name_npz)[:-4]+'.npz', dist=dis, theta=thet, omega=omega, phi=phi)
+
+#print(type(temp))
+
+
+
+
